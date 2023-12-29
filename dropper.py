c50be119b1061eb591df94a11f008e858162514a
Shrey Deogade <shrey.deogade@protonmail.com>
2023-12-29 13:29:57 -0500

    Made code more legible, sorted through beginning dictionaries, improved upon search function and dex assignment

--- dropper.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('pokedex.csv')
-print(df.head(5))
-df = df.drop(columns=['Ability I', 'Ability II', 'Hidden Ability', 'EV Worth', 'Gender', 'Egg Group I', 'Egg Group II'])
-print(df.head(5))
